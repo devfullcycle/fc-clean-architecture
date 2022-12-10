2f5e37e3fe7a2cdb2cec75123b638888eec6a86d
André Leoni <andreluizleoni@gmail.com>
2022-12-10 11:56:07 -0300

    implement product use cases

---
+++ src/usecase/product/create/create.product.dto.ts
@@ -0,0 +1,11 @@
+// export interface InputCreateProductDto {
+//   type: string;
+//   name: string;
+//   price: number;
+// }
+
+// export interface OutputCreateProductDto {
+//   id: string;
+//   name: string;
+//   price: number;
+// }
---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,40 @@
+import CreateProductUseCase from "./create.product.usecase";
+const productCreateTestingInput = {
+  type: "a",
+  name: "my testing product name",
+  price: 20000,
+};
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test create product use case", () => {
+  it("should create a product", async () => {
+    const productRepository = MockRepository();
+    const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+    const output = await productCreateUseCase.execute(productCreateTestingInput);
+
+    expect(output).toEqual({
+      name: "my product name",
+      price: 20000,
+  });
+
+  it("should thrown an error when name is missing", async () => {
+    const productRepository = MockRepository();
+    const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+    productCreateTestingInput.name = "";
+
+    await expect(productCreateUseCase.execute(productCreateTestingInput)).rejects.toThrow(
+      "Name is required"
+    );
+  });
+  });
+});
