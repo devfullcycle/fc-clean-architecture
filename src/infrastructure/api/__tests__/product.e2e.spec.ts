ff80b74188133ab060c209a50d0a1b86404424de
luizhenriqque18 <luizhenriqque15@gmail.com>
2023-03-21 21:25:04 -0400

    test: fix and add test e2e

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -13,9 +13,17 @@ describe("E2E test for product", () => {
   it("should create a product", async () => {
     const response = await request(app).post("/product").send({
       name: "Product 1",
+      price: 100,
     });
 
     expect(response.status).toBe(200);
     expect(response.body.name).toBe("Product 1");
+  });
+  it("should throw an error when name is missing", async () => {
+    const response = await request(app).post("/product").send({
+      price: 100,
+    });
+
+    expect(response.status).toBe(501);
   });
 });
