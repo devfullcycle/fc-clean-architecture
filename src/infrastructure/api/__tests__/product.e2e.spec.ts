5c4f0a6920aa7e61d39c330142ee50f4c494ab9c
luizhenriqque18 <luizhenriqque15@gmail.com>
2023-03-21 20:43:03 -0400

    test: should create a product

---
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,21 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E test for product", () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const response = await request(app).post("/product").send({
+      name: "Product 1",
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("Product 1");
+  });
+});
