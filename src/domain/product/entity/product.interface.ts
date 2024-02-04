ab86a4fb2b2635460504b18a45f7f929591fb7e9
Geovany Mendes <geovany.c.mendes@gmail.com>
2024-02-04 16:53:35 -0300

    feat: update product

--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -2,4 +2,7 @@ export default interface ProductInterface {
   get id(): string;
   get name(): string;
   get price(): number;
+  changeName(name: string): void;
+  changePrice(price: number): void;
+  validate(): boolean;
 }
---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,30 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import UpdateProductUseCase from "./update.product.usecase";
+
+const product = ProductFactory.create("a", "Book", 25.0);
+
+const input = {
+  id: product.id,
+  name: product.name,
+  price: product.price
+};
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test for product update use case", () => {
+  it("should update a product", async () => {
+    const productRepository = MockRepository();
+    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+
+    const output = await productUpdateUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
